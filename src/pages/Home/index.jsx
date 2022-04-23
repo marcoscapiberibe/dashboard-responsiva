281b88a6a24ba5eacdd2b259ad9da36e489d24b4
Marcos Capiberibe <marcoscapiberibe@hotmail.com>
2022-04-23 13:49:30 -0300

    Estruturação da Página Home
    Estruturação da Página Home

---
+++ src/pages/Home/index.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import "./Home.css";
+
+const Home = () => {
+    return (
+        <>
+            <section>
+                <div>
+                    Teste
+                        
+                    
+                </div>
+            </section>
+        </>
+    )
+}
+
+export default Home;
